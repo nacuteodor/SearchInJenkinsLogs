2c460c649e9a48650e89d0949ec7b7ad2aeeb97d
Teodor Nacu <teodor.nacu@softvision.com>
2025-09-15 18:44:19 +0300

    Fixed generating the triage report when there is a Jira issue.

--- src/main/java/ToolArgs.java
+++ src/main/java/ToolArgs.java
@@ -409,6 +409,10 @@ public class ToolArgs implements Cloneable {
             HttpResponse response = Main.getUrlHttpResponse(request, jiraUsername, jiraPassword);
             String pageResp = IOUtils.toString(response.getEntity().getContent());
             System.out.println("Jira call response code: " + response.getStatusLine().getStatusCode());
+            if (response.getStatusLine().getStatusCode() != 200) {
+                System.out.println("Jira call response error: " + pageResp);
+                return issueDescriptionMap;
+            }
             List<Map<String, Object>> issues = JsonPath.read(pageResp, "$.issues[*]");
             for (Map<String, Object> issue : issues) {
                 String issueId = (String) issue.get("key");
